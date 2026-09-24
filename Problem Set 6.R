6390fae27095cac34a6656eb97fb910788f9867c
kerriroseriley <131184659+kerriroseriley@users.noreply.github.com>
2026-09-24 12:18:55 -0400

    Update Problem Set 6.R

--- Problem Set 6.R
+++ Problem Set 6.R
@@ -3,9 +3,16 @@
 gss <- read_dta("~/Documents/PAI741/Problem Set 6/Data/gss7224_r1.dta")
 
 # Figure 1: 0-15 GSS Modified Stouffer Tolerance Battery, 1976-2024
-plot(year, y,type = "l",
+
+# Create plot
+plot(year, y,
+     # Setting the plot type to a line plot
+     type = "l",
+     # title of the figure
      main = "0-15 GSS Modified Stouffer Tolerance Battery, 1976-2024",
+     # x-axis label
      xlab = "Year",
+     # y-axis label
      ylab = "Tolerance Battery")
 
 
