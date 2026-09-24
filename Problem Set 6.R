2ba30c9aec0822f4569df156a86a9346c29d1a29
kerriroseriley <131184659+kerriroseriley@users.noreply.github.com>
2026-09-24 11:38:12 -0400

    Update Problem Set 6.R

--- Problem Set 6.R
+++ Problem Set 6.R
@@ -2,15 +2,15 @@
 # Read the GSS data
 gss <- read_dta("~/Documents/PAI741/Problem Set 6/Data/gss7224_r1.dta")
 
-# Figure 1
-
-# Figure 2
+# Figure 1: 0-15 GSS Modified Stouffer Tolerance Battery, 1976-2024
 
 
-# Figure 5
+# Figure 2: Intolerance Towards Racists and Homosexuals, 1976-1998
 
 
-# Extend analysis to include more recent years
+# Figure 5: A dichotomous Measure of Tolerance and Intolerance, 1976-2024
 
 
-# Original Work: explore the codebook for the GSS. Find two interesting variables and create compelling univariate graphs to illustrate their central tendency, distribution, and spread.
+
+
+# Original Work: explore the code book for the GSS. Find two interesting variables and create compelling univariate graphs to illustrate their central tendency, distribution, and spread.
