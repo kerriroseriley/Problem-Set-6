28ea4dae39744d2a10335d7743e45eea0a220390
kerriroseriley <131184659+kerriroseriley@users.noreply.github.com>
2026-09-24 11:35:01 -0400

    Update Problem Set 6.R

--- Problem Set 6.R
+++ Problem Set 6.R
@@ -1,3 +1,16 @@
 
 # Read the GSS data
 gss <- read_dta("~/Documents/PAI741/Problem Set 6/Data/gss7224_r1.dta")
+
+# Figure 1
+
+# Figure 2
+
+
+# Figure 5
+
+
+# Extend analysis to include more recent years
+
+
+# Original Work: explore the codebook for the GSS. Find two interesting variables and create compelling univariate graphs to illustrate their central tendency, distribution, and spread.
