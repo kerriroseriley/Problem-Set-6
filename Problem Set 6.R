974256404131ef9e406ff0088f7561735ddb9178
kerriroseriley <131184659+kerriroseriley@users.noreply.github.com>
2026-09-24 11:51:51 -0400

    a

--- Problem Set 6.R
+++ Problem Set 6.R
@@ -3,13 +3,25 @@
 gss <- read_dta("~/Documents/PAI741/Problem Set 6/Data/gss7224_r1.dta")
 
 # Figure 1: 0-15 GSS Modified Stouffer Tolerance Battery, 1976-2024
-
+plot(x, y,
+     type = "l",
+     xlab = "X-axis",
+     ylab = "Y-axis",
+     main = "Line Plot")
 
 # Figure 2: Intolerance Towards Racists and Homosexuals, 1976-1998
-
+plot(x, y,
+     type = "l",
+     xlab = "X-axis",
+     ylab = "Y-axis",
+     main = "Line Plot")
 
 # Figure 5: A dichotomous Measure of Tolerance and Intolerance, 1976-2024
-
+plot(x, y,
+     type = "l",
+     xlab = "X-axis",
+     ylab = "Y-axis",
+     main = "Line Plot")
 
 
 
